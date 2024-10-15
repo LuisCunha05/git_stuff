130f0d85ce80d012a277c9969cbe455bfab607fb
LuisCunha05 <luisf.cunha05@gmail.com>
2024-10-15 09:10:13 -0400

    corrigido palindrome hehe

--- palin.py
+++ palin.py
@@ -1,7 +1,9 @@
 def isPalindrome(word: str) -> bool:
     if(type(word) != str):
         raise TypeError
-    
+
+    word = word.replace(' ', '')
+
     for i in range(len(word) // 2):
         if(word[i] != word[-(i + 1)]):
             return False
