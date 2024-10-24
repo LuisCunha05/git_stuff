b127c66906f37f5d25035818d12d0356b84db03f
LuisCunha05 <luisf.cunha05@gmail.com>
2024-10-24 11:13:11 -0400

    Quase finalizado class Biblioteca✌✌

--- biblioteca.py
+++ biblioteca.py
@@ -1,3 +1,6 @@
+from typing import Literal
+from __future__ import annotations
+
 class Usuario:
     def __init__(self, nome: str, cpf: str) -> None:
         self.nome = nome
@@ -11,60 +14,102 @@ class Usuario:
     
     def adicionarLivro(self, titulo: str):
         if(titulo in self.livros):
-            raise ValueError(f'O livro: {titulo} já está emprestado')
+            print(f'O livro: {titulo} já está emprestado')
+            return False
         
         if(len(self.livros) >= self.getMaxEmprestimo()):
-            raise ValueError(f'O usuário já possui o máximo de livros emprestados!')
+            print(f'O usuário já possui o máximo de livros emprestados!')
+            return False
 
         self.livros.append(titulo)
+        return True
+    
+    def removerLivro(self, titulo: str) -> bool:
+        if(len(self.livros) == 0):
+            print('O usuario não possui livros para remover!')
+            return False
+        if(titulo not in self.livros):
+            print('O usuário não possui este livro emprestado!')
+            return False
 
-        return self
+        del self.livros[self.livros.index(titulo)]
+        return True
     
 class Livro:
-    def __init__(self, titulo: str, autor: str, numero_paginas: int, capa_dura: bool, quantidade: int) -> None:
+    def __init__(self, titulo: str, autor: str, numero_paginas: int, codigo: str,status: Literal['Disponivel', 'Emprestado'] = 'Disponivel') -> None:
         self.titulo = titulo
         self.autor = autor
         self.numero_paginas = numero_paginas
-        self.capa_dura = capa_dura
-        self.__quantidade = quantidade
+        self.codigo = codigo
+        self.status: Literal['Disponivel', 'Emprestado'] = status
+        self.usuario: Usuario = None
 
-    def getQuantidade(self) -> int:
-        return self.__quantidade
+    def emprestarLivro(self, usuario: Usuario):
+        if(self.status != 'Disponivel'):
+            print('Livro já está emprestado!')
+            return False
+        self.status = 'Emprestado'
+        self.usuario = usuario
+        return True
     
-    def setQuantidade(self, quantidade: int):
-        """Utilizado para adicionar ou remover livros. Quantidade pode ser negativo. Raises ValueError se o resultado de adicionar a quantidade resulta em um número negativo"""
-        if(self.getQuantidade() + quantidade < 0):
-            raise ValueError('Livro não pode ter uma quantidade negativa')
-
-        self.__quantidade += quantidade
+    def devolverLivro(self):
+        if(self.status != 'Emprestado'):
+            print('Livro não está emprestado!')
+            return False
+        self.status = 'Disponivel'
+        self.usuario = None
+        return True
     
     def __str__(self) -> str:
-        return f"""
-            Título: {self.titulo},
-            Autor: {self.autor},
-            Número de páginas: {self.numero_paginas},
-            Capa dura: {'Sim' if self.capa_dura else 'Não'},
-            Quantidade disponivel: {self.__quantidade}
-        """
+        return f'{self.titulo}, {self.autor}'
 
 
-class Livros:
+class Biblioteca:
     Acervo: list[Livro] = []
 
-    def __init__(self) -> None:
-        pass
+    def cadastrar(self, livro: Livro) -> bool:
+        if(livro in self.Acervo):
+            print("Livro já está cadastrado!")
+            return False
+            
 
-    @staticmethod
-    def cadastrar(livro: Livro):
-        Livros.Acervo.append(livro)
+        self.Acervo.append(livro)
 
-    @staticmethod
-    def listar():
-        for idx, book in enumerate(Livros.Acervo):
-            print(f'Livro {idx}: {book.titulo}')
+    def listar(self):
+        for idx, book in enumerate(self.Acervo):
+            print(f'Livro {idx}: {book}')
     
-    @staticmethod
-    def getAtributos(index: int):
-        print(Livros.Acervo[index])
+    def getAtributos(self, index: int):
+        livro = self.Acervo[index]
+
+        return f"""
+            {livro.titulo},
+            {livro.autor},
+            {livro.status},
+            {livro.codigo},
+            {livro.usuario}
+        """
     
-    
+    def fazerEmprestimo(self, usuario: Usuario, livro: Livro) -> bool:
+        if(len(usuario.livros) == usuario.getMaxEmprestimo()):
+            print('O usuário já atingiu o maximo de emprestimos')
+            return False
+        
+        if(livro.status != 'Disponivel'):
+            print('O livro não pode ser emprestado!')
+            return False
+        
+        usuario.adicionarLivro(livro.titulo)
+        livro.emprestarLivro(usuario)
+
+    def fazerDevolucao(self, usuario: Usuario, livro: Livro) -> bool:
+        if(len(usuario.livros) == 0):
+            print('O usuário não possui livros para devolver')
+            return False
+        
+        if(livro.status != 'Emprestado'):
+            print('O livro não está emprestado!')
+            return False
+        
+        usuario.adicionarLivro(livro)
+        livro.devolverLivro()
