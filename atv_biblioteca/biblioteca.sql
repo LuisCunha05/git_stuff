6a7af698891edc062e1a78faf49211903f13e4e1
LuisCunha05 <luisf.cunha05@gmail.com>
2024-11-06 22:38:10 -0300

    Adicionado LivroBuilder

--- atv_biblioteca/biblioteca.sql
+++ atv_biblioteca/biblioteca.sql
@@ -14,7 +14,7 @@ create table livro (
     titulo VARCHAR(50),
     autor VARCHAR(50),
     genero VARCHAR(50),
-    status_livro VARCHAR(50),
+    status_livro TINYINT, -- 1:disponivel,2-emprestado,3-extraviado,4-danificado
     isbn VARCHAR(50) UNIQUE
 );
 
@@ -28,11 +28,11 @@ CREATE Table emprestimo (
 );
 
 INSERT INTO livro (titulo, autor, genero, status_livro, isbn) VALUES
-('O Alquimista', 'Paulo Coelho', 'Ficção', 'Disponível', '001'),
-('1984', 'George Orwell', 'Distopia', 'Disponível', '002'),
-('Dom Casmurro', 'Machado de Assis', 'Clássico', 'Disponível', '003'),
-('A Revolução dos Bichos', 'George Orwell', 'Fábula', 'Disponível', '004'),
-('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'Infantil', 'Disponível', '005');
+('O Alquimista', 'Paulo Coelho', 'Ficção', 1, '001'),
+('1984', 'George Orwell', 'Distopia', 1, '002'),
+('Dom Casmurro', 'Machado de Assis', 'Clássico', 1, '003'),
+('A Revolução dos Bichos', 'George Orwell', 'Fábula', 1, '004'),
+('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'Infantil', 1, '005');
 
 INSERT INTO usuario (nome, cpf, telefone) VALUES
 ('Maria Silva', '123.456.789-00', '(11) 98765-4321'),
--- atv_biblioteca/livro.py
+++ atv_biblioteca/livro.py
@@ -1,23 +1,136 @@
-from typing import Literal
 from database import DB
 
 class Livro:
-    def __init__(self, titulo: str, autor: str, genero: str, isbn: str, status: Literal['Disponível', 'Emprestado'] = 'Disponível') -> None:
-        self.titulo = titulo
-        self.autor = autor
-        self.genero = genero
-        self.isbn = isbn
-        self.status: Literal['Disponível', 'Emprestado'] = status
-        self.id_usuario: int = None
+    def __init__(self) -> None:
+        self._titulo: str = ''
+        self._autor: str = ''
+        self._genero: str = ''
+        self._isbn: str = ''
+        self._status: int = None #1:disponivel,2-emprestado,3-extraviado,4-danificado
+        self._id_usuario: int = None
+
+    def setTitulo(self, titulo: str):
+        if(type(titulo) != str):
+            raise TypeError('Tipo esperado: str')
+        if(titulo == ''):
+            raise ValueError('Valor não pode ser vazio')
+
+        self._titulo = titulo
+
+    def setAutor(self, autor: str):
+        if(type(autor) != str):
+            raise TypeError('Tipo esperado: str')
+        if(autor == ''):
+            raise ValueError('Valor não pode ser vazio')
+
+        self._autor = autor
+
+    def setGenero(self, genero: str):
+        if(type(genero) != str):
+            raise TypeError('Tipo esperado: str')
+        if(genero == ''):
+            raise ValueError('Valor não pode ser vazio')
+
+        self._genero = genero
+
+    def setIsbn(self, isbn: str):
+        if(type(isbn) != str):
+            raise TypeError('Tipo esperado: str')
+        if(isbn == ''):
+            raise ValueError('Valor não pode ser vazio')
+
+        self._isbn = isbn
+
+    def setStatus(self, status: int = 1):
+        """Status do livro são: 1:disponivel, 2:emprestado, 3:extraviado, 4:danificado"""
+        if(type(status) != int):
+            raise TypeError('Tipo esperado: int')
+        if(status < 1 or status > 4):
+            raise ValueError('Valor precisa ser [1,4]')
+
+        self._status = status
+
+    def setIdUsuario(self, id_usuario: int):
+        if(type(id_usuario) != int):
+            raise TypeError('Tipo esperado: int')
+        if(id_usuario <= 0):
+            raise ValueError('Valor precisa ser maior que 0')
+
+        self._id_usuario = id_usuario
+
+    def getTitulo(self) -> str:
+        return self._titulo
+
+    def getAutor(self) -> str:
+        return self._autor
+
+    def getGenero(self) -> str:
+        return self._genero
+
+    def getIsbn(self) -> str:
+        return self._isbn
+
+    def getStatus(self) -> int:
+        return self._status
+
+    def getIdUsuario(self) -> int:
+        return self._id_usuario
+    
+    def getAsDb(self) -> tuple:
+        return (self._titulo, self._autor, self._genero, self._isbn, self._status)
 
     def __str__(self) -> str:
-        return f'{self.titulo}, {self.autor}'
+        return f'{self._titulo}, {self._autor}'
 
+    def __iter__(self):
+        return iter((self._titulo, self._autor, self._genero, self._isbn, self._status, self._id_usuario))
+
+class LivroBuilder:
+    def __init__(self) -> None:
+        self._livro = Livro()
+
+    def addTitulo(self, titulo: str):
+        self._livro.setTitulo(titulo)
+        return self
+
+    def addAutor(self, autor: str):
+        self._livro.setAutor(autor)
+        return self
+
+    def addGenero(self, genero: str):
+        self._livro.setGenero(genero)
+        return self
+
+    def addIsbn(self, isbn: str):
+        self._livro.setIsbn(isbn)
+        return self
+
+    def addStatus(self, status: int = 1):
+        self._livro.setStatus(status)
+        return self
+
+    def addIdUsuario(self, id_usuario: int):
+        self._livro.setIdUsuario(id_usuario)
+        return self
+    
+    def build(self):
+        if(self._livro.getTitulo() == ''):
+            raise ValueError(f'O atributo Titulo não pode ser vazio')
+        if(self._livro.getAutor() == ''):
+            raise ValueError(f'O atributo Autor não pode ser vazio')
+        if(self._livro.getGenero() == ''):
+            raise ValueError(f'O atributo Genero não pode ser vazio')
+        if(self._livro.getIsbn() == ''):
+            raise ValueError(f'O atributo Isbn não pode ser vazio')
+        if(self._livro.getStatus() is None):
+            raise ValueError(f'O atributo Status precisa ser um int')
+        if(self._livro.getIdUsuario() is not None and self._livro.getIdUsuario() <= 0):
+            raise ValueError(f'O atributo IdUsuario precisa ser maior que Zero')
+        return self._livro
 
 class GerenciarLivro:
-
     @staticmethod
-    def novaInstanciaLivro(isbn: str) -> Livro:
+    def instanceFromDB(isbn: str) -> Livro:
         try:
             db = DB()
             query = """select titulo,autor,genero,isbn,status_livro from livro where isbn=%s"""
@@ -31,8 +144,21 @@ class GerenciarLivro:
 
     
     @staticmethod
-    def adicionarLivro(titulo: str, autor: str, genero: str, isbn: str, status: Literal['Disponível', 'Emprestado'] = 'Disponível') -> bool:
+    def adicionarLivro(titulo: str, autor: str, genero: str, isbn: str, status: int = 1) -> bool:
         """Adiciona um novo livro ao banco de dados. Verifica primeiro se Isbn ja existe no banco e aborta caso sim. Retorna um Bool com status de sucesso da operação de adição"""
+        try:
+            novo: Livro =(LivroBuilder()
+                            .addTitulo(titulo)
+                            .addAutor(autor)
+                            .addGenero(genero)
+                            .addIsbn(isbn)
+                            .addStatus(status)
+                            .build()
+                        )
+        except (ValueError, TypeError) as e:
+            print(f'Erro ao criar instância de Livro:\n{e}')
+            return False
+
         try:
             db = DB()
             db.exec('select id_livro from livro where isbn=%s', (isbn,))
@@ -44,11 +170,11 @@ class GerenciarLivro:
 
             query = """
                 insert into usuario
-                    (titulo, autor, genero, status_livro, isbn)
+                    (titulo, autor, genero, isbn, status_livro)
                     values (%s,%s,%s,%s,%s)
                 """
 
-            db.exec(query=query, args=(titulo, autor, genero, status, isbn))
+            db.exec(query=query, args=novo.getAsDb())
             db.commit()
             db.close()
             return True
@@ -99,4 +225,7 @@ if __name__ == "__main__":
     db = DB()
     db.exec('select status_livro from livro where isbn=%s', ('003',))
     status, = db.f_one()
-    print(GerenciarLivro.novaInstanciaLivro('006'))
+
+    teste = LivroBuilder().addTitulo('apa').addAutor('caa').addGenero('da').addIsbn('007').addStatus().build()
+
+    print(*teste.getAsDb())
